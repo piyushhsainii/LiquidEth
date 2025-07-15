8c9efc0f7c6750b0109a57ca6d916075aaac23f8
Piyush Saini <134310734+piyushhsainii@users.noreply.github.com>
2025-07-15 20:51:03 +0530

    Added Metadata in the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { config } from "@/config";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import { Toaster } from "sonner";
+import { Head } from "next/document";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,6 +26,41 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+        <title>Liquid ETH</title>
+        <meta
+          name="Liquid ETH"
+          content=" Secure, decentralized, and profitable ETH Staking Protocol. Start
+            earning rewards by staking your ETH today"
+        />
+
+        {/* Open Graph tags */}
+        <meta property="og:title" content="Liquid ETH" />
+        <meta
+          property="og:description"
+          content=" Secure, decentralized, and profitable ETH Staking Protocol. Start
+            earning rewards by staking your ETH today"
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dzow59kgu/image/upload/v1752525947/Screenshot_2025-07-15_021504_c0qrhz.png"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://liquid-eth.vercel.app/" />
+
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Liquid ETH" />
+        <meta
+          name="twitter:description"
+          content=" Secure, decentralized, and profitable ETH Staking Protocol. Start
+            earning rewards by staking your ETH today"
+        />
+        <meta
+          name="twitter:image"
+          content="https://res.cloudinary.com/dzow59kgu/image/upload/v1752525947/Screenshot_2025-07-15_021504_c0qrhz.png"
+        />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
